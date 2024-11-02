8df288d81d94768f1613ffeaf06220fcd08c8b91
quazzz <alan256bros@gmail.com>
2024-11-02 18:02:09 +0200

    unused var

--- components/ExerciseCardDashboard.jsx
+++ components/ExerciseCardDashboard.jsx
@@ -7,6 +7,7 @@ export default function ExerciseCardDashboard({ name, gifUrl, bodypart, target, 
         headers: {'Content-Type' : 'application/json'},
         body: JSON.stringify({id})
       })
+      const a = req.json()
       window.location.reload()
     } catch (error) {
       console.error(error)
