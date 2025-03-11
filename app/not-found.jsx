8360bfb3c47773fc91bf5b7fbf35b2d2f6ca0d0f
= <=>
2025-03-11 19:56:24 +0200

    addinng bg to 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,6 +1,6 @@
 export default function notfound() {
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] justify-items-center min-h-screen  text-white p-8 sm:p-20 gap-6 bg-black">
+    <div className="grid grid-rows-[auto_1fr_auto] justify-items-center min-h-screen  text-white p-8 sm:p-20 gap-6 bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#24243e]">
       <h1 className="font-[family-name:var(--font-geist-mono)] text-6xl sm:text-8xl font-bold ">
         404
       </h1>
